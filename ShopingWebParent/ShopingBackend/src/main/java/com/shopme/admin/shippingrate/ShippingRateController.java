83f971fb8d7bd708eaa10e736d4939664d1c5e3b
shahrukh010 <khanshahrukhbsc@gmail.com>
2023-01-14 15:11:17 +0530

    added new rate

--- ShopingWebParent/ShopingBackend/src/main/java/com/shopme/admin/shippingrate/ShippingRateController.java
+++ ShopingWebParent/ShopingBackend/src/main/java/com/shopme/admin/shippingrate/ShippingRateController.java
@@ -1,12 +1,17 @@
 package com.shopme.admin.shippingrate;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.shopme.admin.paginig.PagingAndSortingHelper;
 import com.shopme.admin.paginig.PagingAndSortingParam;
+import com.shopme.common.entity.Country;
+import com.shopme.common.entity.ShippingRate;
 
 @Controller
 public class ShippingRateController {
@@ -29,5 +34,16 @@ public class ShippingRateController {
 		service.listByPage(pageNum, helper);
 
 		return "shipping_rates/shipping_rates";
+	}
+
+	@GetMapping("shipping_rate/new")
+	public String newRate(Model model) {
+
+		List<Country> listCountries = service.listAllCountry();
+		model.addAttribute("rate", new ShippingRate());
+		model.addAttribute("listCountries", listCountries);
+		model.addAttribute("pageTitle", "New Rate");
+
+		return "shipping_rates/shipping_rate_form";
 	}
 }
