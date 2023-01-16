c05b0cda163ebf82b7b9d9d47b58163eae29a633
shahrukh010 <khanshahrukhbsc@gmail.com>
2023-01-16 12:26:33 +0530

    working on shipping rate edit

--- ShopingWebParent/ShopingBackend/src/main/java/com/shopme/admin/shippingrate/ShippingRateController.java
+++ ShopingWebParent/ShopingBackend/src/main/java/com/shopme/admin/shippingrate/ShippingRateController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.shopme.admin.paginig.PagingAndSortingHelper;
 import com.shopme.admin.paginig.PagingAndSortingParam;
@@ -16,14 +18,14 @@ import com.shopme.common.entity.ShippingRate;
 @Controller
 public class ShippingRateController {
 
-	private String defaultRedirectULR = "redirect:/shipping_rates/page/1?sortField=country&sortDir=asc";
+	private String defaultRedirectURL = "redirect:/shipping_rates/page/1?sortField=country&sortDir=asc";
 	@Autowired
 	private ShippingRateService service;
 
 	@GetMapping("/shipping_rates")
 	public String listFirstPage() {
 
-		return defaultRedirectULR;
+		return defaultRedirectURL;
 	}
 
 	@GetMapping("/shipping_rates/page/{pageNum}")
@@ -36,7 +38,7 @@ public class ShippingRateController {
 		return "shipping_rates/shipping_rates";
 	}
 
-	@GetMapping("shipping_rate/new")
+	@GetMapping("shipping_rates/new")
 	public String newRate(Model model) {
 
 		List<Country> listCountries = service.listAllCountry();
@@ -45,5 +47,26 @@ public class ShippingRateController {
 		model.addAttribute("pageTitle", "New Rate");
 
 		return "shipping_rates/shipping_rate_form";
+	}
+
+	@PostMapping("/shipping_rates/save")
+	public String save(ShippingRate rate, RedirectAttributes redirectAttribute) {
+
+		try {
+
+			service.save(rate);
+			redirectAttribute.addAttribute("messsage", "The shipping rate has been save successfully");
+
+		} catch (ShipingRateAlreadyException ex) {
+
+			redirectAttribute.addAttribute("message", ex.getMessage());
+		}
+		return defaultRedirectURL;
+	}
+
+	@GetMapping("/shipping_rates/edit/{id}")
+	public String edit(@PathVariable(name = "id") Integer id, Model model) {
+
+		return null;
 	}
 }
