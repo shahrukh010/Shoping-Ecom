51736785abade3f3c724835c9a537cb192a60476
shahrukh010 <khanshahrukhbsc@gmail.com>
2023-05-22 18:24:59 +0530

    assing multiple role to user

--- ShopingCommon/src/main/java/com/shopme/common/entity/Role.java
+++ ShopingCommon/src/main/java/com/shopme/common/entity/Role.java
@@ -7,11 +7,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 @Entity
 @Table(name = "roles")
 public class Role {
@@ -68,6 +63,30 @@ public class Role {
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 
 }
---
+++ ShopingWebParent/ShopingBackend/src/test/java/com/shopme/admin/order/UserRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.shopme.admin.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import com.shopme.admin.repository.UserRepository;
+import com.shopme.common.entity.Role;
+import com.shopme.common.entity.User;
+
+@DataJpaTest(showSql = true)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTest {
+
+	@Autowired
+	private UserRepository userRepo;
+
+	@Autowired
+	private TestEntityManager entityManager;
+
+	@Test
+	public void testCreateNewUserWithTwoRole() {
+
+		User user = new User("redhat", "Alex", "Smith", "alex@gmail.com");
+		Role roleEditor = new Role(4l);
+		Role roleAssistant1 = new Role(5l);
+		Role roleAssistant2 = new Role(3l);
+		user.addRole(roleEditor);
+		//here you can one user have same role two times so avoid duplicate object
+		//in role class i have implemented equals(),hasCode()
+		user.addRole(roleAssistant1);
+		user.addRole(roleAssistant2);
+		//or same object multiple times if we not implement equals() and hashCode() then in db
+		//two times will be same role for user 
+//		user.addRole(roleAssistant1);
+		
+		
+
+		User saveUser = userRepo.save(user);
+	}
+
+}
